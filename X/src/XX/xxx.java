ff2146c6e96bfeef52af944dfcba5748c28ea0e8
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-26 23:49:43 -0500

    now

---
+++ X/src/XX/xxx.java
@@ -0,0 +1,29 @@
+package XX;
+
+import java.util.Scanner;
+
+public class xxx {
+
+	public static void main(String[] args) {
+
+
+		int N;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese un numero:");
+		N = sc.nextInt();
+
+		while (N != 0 && N > 0) {
+			System.out.print("ingrese numero ");
+			N = sc.nextInt();
+
+		}
+		if (N == 0) {
+			System.out.println("la suma es");
+			
+			
+		}
+		
+	}
+	}
+
+
